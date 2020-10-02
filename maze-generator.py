2d25ea836d9662257e87edd8c828cddd6d460a51
GustavoRuedaEnriquez <gusgus2208@gmail.com>
2020-10-02 09:04:15 -0500

    Graphic implementation complete.

--- maze-generator.py
+++ maze-generator.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import random
 import pygame
 
 
@@ -49,8 +50,10 @@ COLOR_BLUE  = (0, 0, 255)
 FPS = 30
 
 # CELLS ARRAY
-unvisited_cells = []
-visited_cells   = []
+grid    = []
+stack   = []
+visited = []
+
 
 
 
@@ -76,19 +79,82 @@ def draw_grid (x, y, w, rows, columns) :
             pygame.draw.line(window, COLOR_WHITE, [x, y + w], [x + w, y + w])
             # Right cell margin
             pygame.draw.line(window, COLOR_WHITE, [x + w, y], [x + w, y + w])
+            grid.append((x, y))
             x = x + w
         pygame.display.update()
 
+def draw_current_cell(x, y) :
+    pygame.draw.rect(window, COLOR_GREEN, (x + 1, y + 1, 18, 18), 0)
+    pygame.display.update()
 
-def create_maze_dfs() :
-    print('TODO')
+def draw_next_cell(x, y, w, direction) :
+    # Go to the top cell
+    if(direction == 'up') :
+        pygame.draw.rect(window, COLOR_BLUE, (x + 1, y - w + 1, 19, 39), 0)
+    # Go to the left cell
+    elif(direction == 'left') :
+        pygame.draw.rect(window, COLOR_BLUE, (x - w + 1, y + 1, 39, 19), 0)
+    # Go to the bottom cell
+    elif(direction == 'down') :
+        pygame.draw.rect(window, COLOR_BLUE, (x + 1, y + 1, 19, 39), 0)
+    # Go to the right cell
+    elif(direction == 'right') :
+        pygame.draw.rect(window, COLOR_BLUE, (x + 1, y + 1, 39, 19), 0)
+    pygame.display.update()
 
+def draw_backtracking_cell(x, y):
+    pygame.draw.rect(window, COLOR_BLUE, (x + 1, y + 1, 18, 18), 0)
+    pygame.display.update()
+
+def create_maze_dfs(x, y, w) :
+    draw_current_cell(x, y)
+    stack.append((x, y))
+    visited.append((x, y))
+
+    while(len(stack) > 0) :
+        # Check if neighbors are availablecls
+        available_cells = []
+        if( ((x, y - w) not in visited) and ((x, y - w) in grid) ) :
+            available_cells.append('top')
+        if( ((x - w, y) not in visited) and ((x - w, y) in grid) ) :
+            available_cells.append('left')
+        if( ((x, y + w) not in visited) and ((x, y + w) in grid) ) :
+            available_cells.append('bottom')
+        if( ((x + w, y) not in visited) and ((x + w, y) in grid) ) :
+            available_cells.append('right')
+        # Select a random neighbour
+        if(len(available_cells) != 0) :
+            next_cell = random.choice(available_cells)
+            if(next_cell == 'top') :
+                draw_next_cell(x, y, w, 'up')
+                visited.append((x, y - w))
+                stack.append((x, y - w))
+                y = y - w
+            elif(next_cell == 'left') :
+                draw_next_cell(x, y, w, 'left')
+                visited.append((x - w, y))
+                stack.append((x - w, y))
+                x = x - w
+            elif(next_cell == 'bottom') :
+                draw_next_cell(x, y, w, 'down')
+                visited.append((x, y + w))
+                stack.append((x, y + w))
+                y = y + w
+            elif(next_cell == 'right') :
+                draw_next_cell(x, y, w, 'right')
+                visited.append((x + w, y))
+                stack.append((x + w, y))
+                x = x + w
+        else :
+            x, y = stack.pop()
+            draw_current_cell(x, y)
+            draw_backtracking_cell(x, y)
 
 
 draw_grid (0, 0, CELL_D, GRID_W, GRID_H)
-create_maze_dfs()
+create_maze_dfs(CELL_D, CELL_D, 20)
 
-# run the game loop
+# Run the game loop
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
