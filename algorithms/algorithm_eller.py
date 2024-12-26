1fba370b40d47afc4698f5e2106b636abee31fd8
GustavoRuedaEnriquez <grueda-personal@outlook.com>
2024-12-26 00:57:49 -0600

    Functional Eller's algorithm, TODO: modify matrix

--- algorithms/algorithm_eller.py
+++ algorithms/algorithm_eller.py
@@ -13,61 +13,94 @@ import utils.draw as Draw
 
 # We will store the sets on dictionaries, one will consider all cells from all
 # rows, another one will focus only on the processed row.
-all_rows = dict()
-current_row = dict()
+row_sets = dict()
+non_orphan_cells = list()
 
-def merge_sets(dictionary, set_1_id, set_2_id, set_ids):
-  print("\nWill merge set ", set_1_id, " with set ", set_2_id)
-  print("dict before merging: ", dictionary)
+def merge_sets(set_1_id, set_2_id):
+  row_sets[set_1_id] = row_sets[set_1_id].union(row_sets[set_2_id])
+  del row_sets[set_2_id]  
 
-  dictionary[set_1_id] = dictionary[set_1_id].union(dictionary[set_2_id])
-  del dictionary[set_2_id]
-  set_ids.remove(set_2_id)
+def find_set_with_cell(cell):
+  for key in row_sets.keys():
+    if cell in row_sets[key]:
+      return key
+  return None
+
+def merge_adjacent_sets(window, matrix, width, height, curr_y):
+  keep_processing = True
+  curr_x = 1
+
+  # Since walls also occupies one cell, to check if a cell has neighbors, we
+  # have to move "one distance unit" (odu), which is equal to 2 cells
+  odu = 2
+
+  while keep_processing:
+    # Stop processing when we reach the last column
+    if curr_x >= 2 * width - 1:
+      keep_processing = False
+      break
+
+    # Check if neighbor cell does not belong on same set.
+    set_id = find_set_with_cell((curr_x, curr_y))
+
+
+    # If we are on the last row, we need to connect all adjacent cells
+    if curr_y == 2 * height - 1:
+      if (curr_x + odu, curr_y) not in row_sets[set_id]:
+        other_set_id = find_set_with_cell((curr_x + odu, curr_y))
+        merge_sets(set_id, other_set_id)
+        start = (curr_x, curr_y)
+        end = (curr_x + odu, curr_y)
+        Draw.draw_connecting_cells(window, matrix, start, end, Draw.COLOR_CYAN)
+      else:
+        curr_x += odu
+
+    # Can we join these 2 neighbor cells?
+    elif (curr_x + odu, curr_y) not in row_sets[set_id]:
+      # Are we going to merge the cells (union both sets)?
+      merge_set_factor = random.randint(1,10)
+      if merge_set_factor > 4:
+        other_set_id = find_set_with_cell((curr_x + odu, curr_y))
+        merge_sets(set_id, other_set_id)
+        start = (curr_x, curr_y)
+        end = (curr_x + odu, curr_y)
+        Draw.draw_connecting_cells(window, matrix, start, end, Draw.COLOR_CYAN)
+      else:
+        curr_x += odu
   
-  print("dict after merging: ", dictionary)
-  
-
-def is_set_on_right_end(cell_set, width):
-  last_right_cell = (1,1)
-  #print(last_right_cell[0])
-  for cell in cell_set:
-    #print(cell[0])
-    if cell[0] > last_right_cell[0]:
-      last_right_cell = cell
-  
-  if last_right_cell[0] == (2 * width -1):
-    return True
-  else:
-    return False
-
-
-def random_merge_adjacent_sets(width):
-  set_ids = list(current_row.keys())
-  sets_on_row = len(set_ids)
-
-  # Determine how many sets are going to be merged
-  sets_to_merge = random.randint(1, sets_on_row - 1)
-  print("Will do ", sets_to_merge, " merge(s)\n")
-
-  while sets_to_merge > 0:
-    print(set_ids)
-    # Randomly choose a set id
-    id = random.choice(set_ids)
-    id_idx = set_ids.index(id)
-
-    # Check if the set is located on the right end, if this is the case, we will
-    # merge with the set from the left, otherwise we merge with the set from the
-    # right
-    if is_set_on_right_end(current_row[id], width):
-      # Merge with left set
-      merge_sets(current_row, set_ids[id_idx], set_ids[id_idx - 1], set_ids)
     else:
-      # Merge with right set
-      merge_sets(current_row, set_ids[id_idx], set_ids[id_idx + 1], set_ids)
-    
-    sets_to_merge -= 1
+      curr_x += odu
 
-def execute_eller_algorithm(window, maze_matrix, width, height):
+  # TODO: Modify matrix.  
+
+def choose_cells_from_below(window, matrix):
+  # For each set, choose random cells from next row
+  for key in row_sets.keys():
+    # Set a random number of openings
+    num_cells = random.randint(1, len(row_sets[key]))
+    chosen_cells = list()
+    # Remove all coordinates  from the cell, since their row has already been
+    # processed.
+    while len(row_sets[key]) > 0:
+      cell = row_sets[key].pop()
+      non_orphan_cells.remove(cell)
+      if num_cells > 0:
+        chosen_cells.append(cell)
+        num_cells -= 1
+
+    # Add the cells from below chosen to the same set. Since walls also occupies
+    # one cell, to point to a cell on the next row we have to move "one distance
+    # unit" (odu), which is equal to 2 cells
+    odu =  2
+    for i in range(0, len(chosen_cells)):
+      x = chosen_cells[i][0]
+      y = chosen_cells[i][1]
+      row_sets[key].add((x, y + odu))
+      non_orphan_cells.append((x, y + odu))
+      Draw.draw_connecting_cells(window, matrix, (x, y), (x,y+odu), Draw.COLOR_CYAN)
+
+
+def execute_eller_algorithm(window, matrix, width, height):
   set_id = 1
 
   current_x = 1
@@ -77,15 +110,28 @@ def execute_eller_algorithm(window, maze_matrix, width, height):
   # Slot cells are always odd cells on X and odd on Y.
   for i in range (1, width + 1):
     current_x = 2 * i - 1
-    all_rows[set_id] = {(current_x, current_y)}
-    current_row[set_id] = {(current_x, current_y)}
+    row_sets[set_id] = {(current_x, current_y)}
+    non_orphan_cells.append((current_x, current_y))
     set_id += 1
   
-  #print("all_rows\n",all_rows)
-  #print("current_row\n",current_row)
-
-  # Randomly merge adjacent cells that belong to different sets
-  random_merge_adjacent_sets(width)
+  for i in range (0, height):
+    merge_adjacent_sets(window, matrix, width, height, current_y)
+    #time.sleep(6)
+    # Skip these steps when being on the last row
+    if (current_y < 2 * height - 1):
+      choose_cells_from_below(window, matrix)
+      #time.sleep(6)
+      current_y += 2
+      # Add any possible orphan cell to a unique set
+      for i in range (1, width + 1):
+       current_x = 2 * i - 1
+       if (current_x, current_y) not in non_orphan_cells:
+          row_sets[set_id] = {(current_x, current_y)}
+          non_orphan_cells.append((current_x, current_y))
+          set_id += 1
+          
+  Draw.draw_start_end_cells(window, matrix, width, height)
+  
 
 def generate_maze(window, maze_matrix, width, height) :
   Draw.draw_maze_matrix(window, maze_matrix, Draw.COLOR_WHITE)
